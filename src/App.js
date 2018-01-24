85704aa1546980ee30dc468e7b7e27570ec6d3c6
xgirma <xgirma@gmail.com>
2018-01-24 00:14:21 -0800

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <p>Hello</p>
+  </div>
+);
 
 export default App;
