6f769ecbdd63de251945b353dde68d03df41642b
xgirma <xgirma@gmail.com>
2018-01-25 02:56:30 -0800

    update prop types

--- src/types/index.js
+++ src/types/index.js
@@ -1,21 +1,23 @@
-import { shape, number, string, oneOf } from 'prop-types';
+import PropTypes, { shape, number, string, oneOf } from 'prop-types';
 
-const itemsType = shape({
+const items = shape({
   id: number.isRequired,
   name: string.isRequired,
   price: number.isRequired,
   description: string.isRequired,
 }).isRequired;
+
+const itemsType = PropTypes.arrayOf(items);
 
 const localeType = shape({
   country: oneOf(['US', 'UK', 'BE']),
 }).isRequired;
 
-const itemType = {
+const itemType = shape({
   id: number.isRequired,
   name: string.isRequired,
   price: number.isRequired,
   description: string.isRequired,
-};
+});
 
 export { itemsType, localeType, itemType };
