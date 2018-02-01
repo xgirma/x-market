ea00052d9c18f0986863695c36a0fb3ac67d8d0b
xgirma <xgirma@gmail.com>
2018-01-31 19:55:45 -0800

    rearange component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,58 @@
-import React from 'react';
-import LocalizationBox from './component/LocalizationBox';
-import { itemsType, localeType } from './types';
-import { itemsDefault, localeDefault } from './data/default';
+import React, { Component } from 'react';
+import { itemsType, localeType, translateType } from './types';
+import { itemsDefault, localeDefault, translateDefault } from './data/default';
+import LocalizationBox from './component/Localization';
+import CartItemList from './component/CartItemList';
+import TotalBox from './component/TotalBox';
+import Translate from './lib/Translate';
 
-function App({ items, locale }) {
-  return <div className="App"> <LocalizationBox items={items} locale={locale} /> </div>;
+class Application extends Component {
+	state = {
+	  items: this.props.items,
+	  locale: this.props.locale,
+	  translate: new Translate('US').translate,
+	};
+
+	handleLocale = (newLocale) => {
+	  this.setState(state => ({ locale: newLocale }));
+	  this.setState(state => ({ translate: new Translate(newLocale.country).translate }));
+	};
+
+	removeItem = (itemToRemove) => {
+	  this.setState({
+	    items: this.state.items.filter(item => item.id !== itemToRemove.id),
+	  });
+	};
+
+	resetItems = () => {
+	  this.setState({
+	    items: this.props.items,
+	  });
+	};
+
+	render() {
+	  const { translate } = this.state;
+	  return (
+  <div className="App">
+    <h2>X Market</h2>
+
+    <LocalizationBox translate={translate} onLocaleChange={this.handleLocale} />
+    <CartItemList {...this.state} onRemove={this.removeItem} />
+    <TotalBox {...this.state} onReset={this.resetItems} />
+  </div>);
+	}
 }
 
-export default App;
+export default Application;
 
-App.propTypes = {
+Application.propTypes = {
   items: itemsType,
   locale: localeType,
+  translate: translateType,
 };
 
-App.defaultProps = {
+Application.defaultProps = {
   items: itemsDefault,
   locale: localeDefault,
+  translate: translateDefault,
 };
--- src/component/CartItemList.js
+++ src/component/CartItemList.js
@@ -5,26 +5,36 @@ import CartItem from './CartItem';
 import { itemsType, localeType, translateType } from '../types';
 import { itemsDefault, localeDefault, translateDefault } from '../data/default';
 
-const CartItemList = (props) => {
-  const {
-    items, locale, onRemove, translate,
-  } = props;
-
-  return (
-    <div>
-      <div id="item-lst">
-        {items.map(item => (
-          <CartItem
-            key={shortid.generate()}
-            onRemove={() => onRemove(item)}
-            locale={locale}
-            item={item}
-            translate={translate}
-          />))}
+class CartItemList extends React.Component {
+  render() {
+	  const {
+      items, locale, onRemove, translate,
+    } = this.props;
+	  const { length } = this.props.items;
+	  const closing = length === 1 ? `${translate('ITEM')}` : `${translate('ITEMS')}`;
+    return (
+      <div>
+        <div>
+          <h3 id="message">
+            {translate('YOUR_CART')} {length === 0 ?
+		        (`${translate('IS_EMPTY')}`) :
+		        (`${translate('HAS')} ${length} ${closing}`)}
+          </h3>
+        </div>
+        <div id="item-lst">
+          {items.map(item => (
+            <CartItem
+              key={shortid.generate()}
+              onRemove={() => onRemove(item)}
+              locale={locale}
+              item={item}
+              translate={translate}
+            />))}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default CartItemList;
 
